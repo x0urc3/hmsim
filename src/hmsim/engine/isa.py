52bf49f6be6593fa858733fbae2f1927ef7990cd
Khairulmizam Samsudin <xource@gmail.com>
2026-03-06 10:34:10 +0800

    Add HMv2 opcodes to ISA
    - Add OP_SUB (0x6), OP_JMP (0x8), OP_JMPZ (0x9)
    - Create HMV2_ISA dict with inherited HMv1 + new instructions
    - Add VERSION_ISA lookup dict for version-specific ISA access
    - Update get_opcode() and get_cycles() to accept version parameter

--- src/hmsim/engine/isa.py
+++ src/hmsim/engine/isa.py
@@ -10,6 +10,9 @@ from typing import Dict, Tuple
 OP_LOAD = 0x1
 OP_STORE = 0x2
 OP_ADD = 0x5
+OP_SUB = 0x6
+OP_JMP = 0x8
+OP_JMPZ = 0x9
 
 HMV1_ISA: Dict[str, Tuple[int, int]] = {
     "LOAD": (OP_LOAD, 5),
@@ -17,14 +20,26 @@ HMV1_ISA: Dict[str, Tuple[int, int]] = {
     "ADD": (OP_ADD, 10),
 }
 
+HMV2_ISA: Dict[str, Tuple[int, int]] = {
+    **HMV1_ISA,
+    "SUB": (OP_SUB, 10),
+    "JMP": (OP_JMP, 5),
+    "JMPZ": (OP_JMPZ, 5),
+}
+
+VERSION_ISA = {
+    "HMv1": HMV1_ISA,
+    "HMv2": HMV2_ISA,
+}
+
 HMV1_OPCODE_TO_MNEMONIC = {v[0]: k for k, v in HMV1_ISA.items()}
 
 
-def get_opcode(mnemonic: str) -> int:
+def get_opcode(mnemonic: str, version: str = "HMv1") -> int:
     """Get opcode number for a mnemonic."""
-    return HMV1_ISA[mnemonic.upper()][0]
+    return VERSION_ISA[version][mnemonic.upper()][0]
 
 
-def get_cycles(mnemonic: str) -> int:
+def get_cycles(mnemonic: str, version: str = "HMv1") -> int:
     """Get cycle count for a mnemonic."""
-    return HMV1_ISA[mnemonic.upper()][1]
+    return VERSION_ISA[version][mnemonic.upper()][1]
